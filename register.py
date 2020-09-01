24c50856592ba99488db8a3c46c2ddf585c40913
sonyal <slew@umd.edu>
2020-08-31 20:29:38 -0400

    extract class info from schedule of classes. if class has openings, will open class registration site

--- register.py
+++ register.py
@@ -1,8 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
 options = FirefoxOptions()
 options.add_argument("--headless")
-driver = webdriver.Firefox(options=options)
+binary = FirefoxBinary('C:\\Program Files\\Mozilla Firefox\\firefox.exe')
+driver = webdriver.Firefox(options=options,firefox_binary=binary, executable_path=r'D:\\txtbk\\Personal\\geckodriver-v0.27.0-win64\\geckodriver.exe')
 driver.get("https://app.testudo.umd.edu/soc/search?courseId=PSYC123&sectionId=0201&termId=202008&openSectionsOnly=true&_openSectionsOnly=on&creditCompare=&credits=&courseLevelFilter=ALL&instructor=&_facetoface=on&_blended=on&_online=on&courseStartCompare=&courseStartHour=&courseStartMin=&courseStartAM=&courseEndHour=&courseEndMin=&courseEndAM=&teachingCenter=ALL&_classDay1=on&_classDay2=on&_classDay3=on&_classDay4=on&_classDay5=on")
 
-print(driver.find_element_by_class_name("open-seats has-open-seats").text)
+if not driver.find_element_by_class_name("open-seats-count").text == 0:
+    driver.get("https://app.testudo.umd.edu/#/main/dropAdd?termId=202008")
+
+    if "Central Authentication Service (CAS)" in driver.page_source:
